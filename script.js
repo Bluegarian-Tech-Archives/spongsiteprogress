4c07a91117e4cf9721367425ab9c706709746348
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-08-17 14:50:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -17,30 +17,37 @@ let navTextSize = document.getElementById('NAV-BAR-ID');
 let navTextSize2 = document.getElementById('NAV-BAR-IDB');
 let navTextSize3 = document.getElementById('NAV-BAR-IDC');
 
-if (screenWidth < 1262) 
-{
-    serverInfoCont.style.width = '98%';
-};
+    if (screenWidth < 1262) 
+    {
+        serverInfoCont.style.width = '98%';
+    };
 
-if (screenWidth < 490) 
-{
-    navLogo.style.height = '35px';
-    navLogo.style.width = '35px';
-    navLogo.style.borderStyle = 'none';
-    navLogo.style.marginTop = '0.8%';
-};
+    if (screenWidth < 490) 
+    {
+        navLogo.style.height = '35px';
+        navLogo.style.width = '35px';
+        navLogo.style.borderStyle = 'none';
+        navLogo.style.marginTop = '0.8%';
+    };
 
-if (screenWidth < 490) 
-{
-    navTextSize.style.fontSize = '18px';
-};
+    if (screenWidth < 490) 
+    {
+        navTextSize.style.fontSize = '18px';
+    };
 
-if (screenWidth < 490) 
-{
-    navTextSize2.style.fontSize = '18px';
-};
+    if (screenWidth < 490) 
+    {
+        navTextSize2.style.fontSize = '18px';
+    };
 
-if (screenWidth < 490) 
-{
-    navTextSize3.style.fontSize = '18px';
-};
+    if (screenWidth < 490) 
+    {
+        navTextSize3.style.fontSize = '18px';
+    };
+
+navTextSize2.addEventListener('click', function(){
+    alert('Sorry, the server doesn\'t have shop yet, this button is useless!');
+})
+navTextSize3.addEventListener('click', function(){
+    alert('Sorry, the server doesn\'t have voting system yet, this button is useless!');
+})
